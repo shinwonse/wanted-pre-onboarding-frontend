2d8dc0b22962a1a8f81d7b9d5394a0e6500af34b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-04 01:35:11 +0900

    feat: 라우터 설정

---
+++ src/router.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import ToDos from './pages/ToDos';
+import Root from './Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '',
+        element: <Login />,
+      },
+      {
+        path: 'signup',
+        element: <SignUp />,
+      },
+      {
+        path: 'todos',
+        element: <ToDos />,
+      },
+    ],
+  },
+]);
+
+export default router;
