4b6503267ad8f6dd77dde5afd52112e94c42e5df
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-05 20:50:09 +0900

    feat: 이메일 input에 따른 핸들링

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { emailValidation } from '../validations/emailValidation';
 
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(true);
+
+  const handleEmailInput = ({ target }) => {
+    if (emailValidation(target.value)) {
+      setEmailError(false);
+      setEmail(target.value);
+      return;
+    }
+    setEmailError(true);
+    setEmail(target.value);
+  };
+
   return (
     <main className="SignupMain">
       <h1 className="Signup__Title">JOIN</h1>
       <form className="SignupForm">
         <div className="SignupInput">
           <label htmlFor="email">EMAIL</label>
-          <input className="SignupInput__element" type="email" id="email" />
+          <input
+            className="SignupInput__element"
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailInput}
+          />
         </div>
         <div className="SignupInput">
           <label htmlFor="password">PASSWORD</label>
