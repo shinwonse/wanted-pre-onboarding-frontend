dcab46855f03d6a41a2f229a3b32116dd82537ec
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-05 23:29:35 +0900

    feat: login api 호출

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { BASE_URL } from '../constants';
+import { emailValidation } from '../validations/emailValidation';
+import { passwordValidation } from '../validations/passwordValidation';
 
 function Login() {
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(true);
+
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(true);
+
+  const handleEmailInput = ({ target }) => {
+    if (emailValidation(target.value)) {
+      setEmail(target.value);
+      setEmailError(false);
+      return;
+    }
+    setEmail(target.value);
+    setEmailError(true);
+  };
+
+  const handlePasswordInput = ({ target }) => {
+    if (passwordValidation(target.value)) {
+      setPassword(target.value);
+      setPasswordError(false);
+      return;
+    }
+    setPassword(target.value);
+    setPasswordError(true);
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post(`${BASE_URL}/auth/signin`, {
+      email,
+      password,
+    });
+    console.log(data);
+    navigate('/todo');
+  };
 
   const navigateToJoin = (e) => {
     e.preventDefault();
@@ -17,14 +58,30 @@ function Login() {
       <form className="LoginForm">
         <div className="Input">
           <label htmlFor="email">EMAIL</label>
-          <input className="Input__element" type="email" id="email" />
+          <input
+            className="Input__element"
+            type="email"
+            id="email"
+            placeholder="example@ex.com"
+            value={email}
+            onChange={handleEmailInput}
+          />
         </div>
         <div className="Input">
           <label htmlFor="password">PASSWORD</label>
-          <input className="Input__element" type="password" id="password" />
+          <input
+            className="Input__element"
+            type="password"
+            id="password"
+            placeholder="password must be 8 characters or more"
+            value={password}
+            onChange={handlePasswordInput}
+          />
         </div>
         <div className="Button__Wrapper">
-          <button className="Button">LOGIN</button>
+          <button className="Button" onClick={login}>
+            LOGIN
+          </button>
           <button className="Button" onClick={navigateToJoin}>
             CREATE ACCOUNT
           </button>
