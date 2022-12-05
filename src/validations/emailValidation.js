eb1388ff65301900abf96c81722433a2f537ecf4
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-05 20:38:34 +0900

    refactoring: 이메일 validation 수정

--- src/validations/emailValidation.js
+++ src/validations/emailValidation.js
@@ -1,4 +1,4 @@
 export const emailValidation = (email) => {
-  const emailRegex = /@/;
-  return emailRegex.test(email);
+  const emailReg = /^[a-zA-Z\d._-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,4}$/;
+  return emailReg.test(email);
 };
