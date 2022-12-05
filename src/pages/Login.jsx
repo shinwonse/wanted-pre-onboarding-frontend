1e2d821896d53e36189a361f23b2d63f88f5bfc2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-05 16:38:27 +0900

    feat: 로그인 페이지 마크업 완료

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <main className="LoginMain">
+      <div className="Title">
+        <h1>WANTED TODO LIST</h1>
+      </div>
+      <form className="LoginForm">
+        <div className="Input">
+          <label htmlFor="email">EMAIL</label>
+          <input className="Input__element" type="email" id="email" />
+        </div>
+        <div className="Input">
+          <label htmlFor="password">PASSWORD</label>
+          <input className="Input__element" type="password" id="password" />
+        </div>
+        <div className="Button__Wrapper">
+          <button className="Button">LOGIN</button>
+          <button className="Button">CREATE ACCOUNT</button>
+        </div>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
