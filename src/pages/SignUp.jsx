828ed52ca2b076af928ea054a897324d09c90850
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-05 20:06:10 +0900

    feat: signup 마크업 완료

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 function SignUp() {
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <main className="SignupMain">
+      <h1 className="Signup__Title">JOIN</h1>
+      <form className="SignupForm">
+        <div className="SignupInput">
+          <label htmlFor="email">EMAIL</label>
+          <input className="SignupInput__element" type="email" id="email" />
+        </div>
+        <div className="SignupInput">
+          <label htmlFor="password">PASSWORD</label>
+          <input
+            className="SignupInput__element"
+            type="password"
+            id="password"
+          />
+        </div>
+        <div className="Signup__button__wrapper">
+          <button className="Signup__button">JOIN</button>
+        </div>
+      </form>
+    </main>
   );
 }
 
