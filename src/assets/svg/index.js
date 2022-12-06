25426407668801dca46a3c972328f8ff16bf0283
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-06 22:02:13 +0900

    feat: svg 컴포넌트 export

---
+++ src/assets/svg/index.js
@@ -0,0 +1,5 @@
+export { ReactComponent as Plus } from './icons/icon-plus.svg';
+export { ReactComponent as Edit } from './icons/icon-edit.svg';
+export { ReactComponent as Delete } from './icons/icon-delete.svg';
+export { ReactComponent as Complete } from './icons/icon-complete.svg';
+export { ReactComponent as Back } from './icons/icon-back.svg';
